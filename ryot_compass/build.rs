25418756abedde588c582ca7d79cea6d779ff4d6
lgrossi <lucas.ggrossi@gmail.com>
2024-01-20 14:21:22 +0100

    ci: Add CI build for github pages

--- ryot_compass/build.rs
+++ ryot_compass/build.rs
@@ -6,12 +6,16 @@ pub use build_scripts::*;
 use std::env;
 
 fn main() {
+    println!("cargo:warning=Build script of ryot_compass is running...");
+
     // Check if the SKIP_BUILD_SCRIPT environment variable is set
     if env::var("SKIP_BUILD_SCRIPT").is_ok() {
-        println!("Skipping build script for CI build");
+        println!("cargo:warning=Skipping ryot_compass build script for CI build");
         return;
     }
 
     build_target::run();
     content_builder::run().expect("Failed to build assets");
+
+    println!("cargo:warning=Build script of ryot_compass completed.");
 }
