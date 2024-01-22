efc3037696747477d6a433e47b4e29a801bf1967
lgrossi <lucas.ggrossi@gmail.com>
2024-01-22 02:03:36 +0100

    impr: Move all custom errors to thiserror format

--- ryot/src/sprites/error.rs
+++ ryot/src/sprites/error.rs
@@ -1,34 +1,18 @@
+use thiserror::Error;
+
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
-    Io(std::io::Error),
-    Serde(serde_json::Error),
-    Lzma(lzma_rs::error::Error),
-    Image(image::ImageError),
+    /// An [IO Error](std::io::Error)
+    #[error("Could not read the file: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Could not serialize/deserialize file: {0}")]
+    Serde(#[from] serde_json::Error),
+    #[error("Could not decompress file: {0}")]
+    Lzma(#[from] lzma_rs::error::Error),
+    #[error("Invalid image: {0}")]
+    Image(#[from] image::ImageError),
+    #[error("Could not find sprite.")]
     SpriteNotFound,
-}
-
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Self {
-        Error::Io(e)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(e: serde_json::Error) -> Self {
-        Error::Serde(e)
-    }
-}
-
-impl From<lzma_rs::error::Error> for Error {
-    fn from(e: lzma_rs::error::Error) -> Self {
-        Error::Lzma(e)
-    }
-}
-
-impl From<image::ImageError> for Error {
-    fn from(e: image::ImageError) -> Self {
-        Error::Image(e)
-    }
 }
--- ryot_compass/src/error.rs
+++ ryot_compass/src/error.rs
@@ -1,26 +1,11 @@
-use std::fmt::{Display, Formatter};
 use std::result;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
-    DatabaseError(String),
+    #[cfg(all(feature = "lmdb", not(target_arch = "wasm32")))]
+    #[error("Database error: {0}")]
+    DatabaseError(#[from] heed::Error),
 }
-
-#[cfg(all(feature = "lmdb", not(target_arch = "wasm32")))]
-impl From<heed::Error> for Error {
-    fn from(e: heed::Error) -> Self {
-        Error::DatabaseError(e.to_string())
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::DatabaseError(e) => write!(f, "Database error: {}", e),
-        }
-    }
-}
-
-impl std::error::Error for Error {}
 
 pub type Result<T> = result::Result<T, Error>;
