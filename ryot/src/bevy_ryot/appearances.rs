4f92ece1552ad7873596257e8c1b45dc34ba960f
lgrossi <lucas.ggrossi@gmail.com>
2024-01-30 19:58:03 +0100

    feat: Break ContentAssets into pieces and introduce appearances preparer

--- ryot/src/bevy_ryot/appearances.rs
+++ ryot/src/bevy_ryot/appearances.rs
@@ -2,11 +2,14 @@
 //! This module contains the code to load the appearances.dat file.
 //! This file contains the information needed to load sprites and other content.
 
+use crate::appearances;
 use crate::appearances::Appearances;
+use crate::bevy_ryot::ContentAssets;
 use bevy::asset::io::Reader;
 use bevy::asset::{Asset, AssetLoader, AsyncReadExt, BoxedFuture, LoadContext};
 use bevy::prelude::*;
 use bevy::reflect::TypeUuid;
+use bevy::utils::HashMap;
 use prost::Message;
 use thiserror::Error;
 
@@ -65,4 +68,92 @@ impl AssetLoader for AppearanceAssetLoader {
     fn extensions(&self) -> &[&str] {
         &["dat"]
     }
+}
+
+#[derive(Debug, Default)]
+pub struct PreparedAppearances {
+    pub objects: HashMap<u32, appearances::Appearance>,
+    pub outfits: HashMap<u32, appearances::Appearance>,
+    pub effects: HashMap<u32, appearances::Appearance>,
+    pub missiles: HashMap<u32, appearances::Appearance>,
+}
+
+/// Prepares the appearances from the .dat file into a HashMap to allow fast access
+/// to the appearances by id. It keeps the appearances in their original separation:
+/// objects, outfits, effects, missiles and special.
+///
+/// A prepared appearance must have at least an id and a main sprite id.
+/// Appearances that don't have at least these two fields are ignored.
+pub(crate) fn prepare_appearances<C: ContentAssets>(
+    mut content_assets: ResMut<C>,
+    appearances: Res<Assets<Appearance>>,
+) {
+    info!("Preparing appearances");
+    let Some(Appearance(appearances)) = appearances.get(content_assets.appearances().id()) else {
+        panic!("No config found for content");
+    };
+
+    let prepared_appearances = content_assets.prepared_appearances_mut();
+
+    for (kind, from, to) in [
+        (
+            "object",
+            &appearances.object,
+            &mut prepared_appearances.objects,
+        ),
+        (
+            "outfit",
+            &appearances.outfit,
+            &mut prepared_appearances.outfits,
+        ),
+        (
+            "effect",
+            &appearances.effect,
+            &mut prepared_appearances.effects,
+        ),
+        (
+            "missile",
+            &appearances.missile,
+            &mut prepared_appearances.missiles,
+        ),
+    ] {
+        process_appearances(from, |id, appearance| {
+            to.insert(id, appearance.clone());
+        });
+        info!("{} out of {} '{}' prepared", to.len(), from.len(), kind);
+    }
+
+    info!("Appearances prepared");
+}
+
+fn process_appearances(
+    appearances: &[appearances::Appearance],
+    mut insert_fn: impl FnMut(u32, &appearances::Appearance),
+) {
+    appearances
+        .iter()
+        .filter_map(|appearance| {
+            if appearance.frame_group.is_empty() {
+                return None;
+            }
+
+            let Some(id) = appearance.id else {
+                return None;
+            };
+
+            for frame_group in appearance.frame_group.iter() {
+                let Some(sprite_info) = &frame_group.sprite_info else {
+                    continue;
+                };
+
+                if sprite_info.sprite_id.is_empty() {
+                    continue;
+                }
+
+                break;
+            }
+
+            Some((id, appearance))
+        })
+        .for_each(|(id, appearance)| insert_fn(id, appearance));
 }
--- ryot_compass/src/bevy_compass/assets.rs
+++ ryot_compass/src/bevy_compass/assets.rs
@@ -3,7 +3,8 @@ use bevy::prelude::{Image, Resource, TextureAtlas};
 use bevy::utils::HashMap;
 use bevy_asset_loader::asset_collection::AssetCollection;
 use ryot::appearances::SpriteSheetDataSet;
-use ryot::bevy_ryot::{Appearance, Catalog, ConfigAsset, ContentAssets};
+use ryot::bevy_ryot::{Appearance, Catalog, ConfigAsset, ContentAssets, PreparedAppearances};
+use ryot::prelude::*;
 use ryot::ContentConfigs;
 
 #[derive(Default)]
@@ -23,6 +24,7 @@ pub struct CompassContentAssets {
     catalog_content: Handle<Catalog>,
     #[asset(path = "config/.content.toml")]
     config: Handle<ConfigAsset<ContentConfigs>>,
+    prepared_appearances: PreparedAppearances,
 
     // Image related handles
     #[asset(path = "ryot_mascot.png")]
@@ -34,11 +36,22 @@ pub struct CompassContentAssets {
     sprite_sheets: HashMap<String, Handle<Image>>,
 }
 
-impl ContentAssets for CompassContentAssets {
+impl ContentAssets for CompassContentAssets {}
+impl AppearancesAssets for CompassContentAssets {
     fn appearances(&self) -> &Handle<Appearance> {
         &self.appearances
     }
 
+    fn prepared_appearances(&self) -> &PreparedAppearances {
+        &self.prepared_appearances
+    }
+
+    fn prepared_appearances_mut(&mut self) -> &mut PreparedAppearances {
+        &mut self.prepared_appearances
+    }
+}
+
+impl ConfigAssets for CompassContentAssets {
     fn catalog_content(&self) -> &Handle<Catalog> {
         &self.catalog_content
     }
@@ -46,7 +59,9 @@ impl ContentAssets for CompassContentAssets {
     fn config(&self) -> &Handle<ConfigAsset<ContentConfigs>> {
         &self.config
     }
+}
 
+impl SpriteAssets for CompassContentAssets {
     fn sprite_sheets(&self) -> &HashMap<String, Handle<Image>> {
         &self.sprite_sheets
     }
@@ -54,6 +69,7 @@ impl ContentAssets for CompassContentAssets {
     fn sprite_sheet_data_set(&self) -> &Option<SpriteSheetDataSet> {
         &self.atlas.sheet_data_set
     }
+
     fn set_sprite_sheets_data(&mut self, sprite_sheet_set: SpriteSheetDataSet) {
         self.atlas.sheet_data_set.replace(sprite_sheet_set);
     }
