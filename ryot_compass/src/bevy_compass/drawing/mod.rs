7d21d5dc07ea6e162613f48c27353bd41c37425d
lgrossi <lucas.ggrossi@gmail.com>
2024-02-08 15:05:56 +0100

    impr: Breaking down the ryot_compass:drawing mod

---
+++ ryot_compass/src/bevy_compass/drawing/mod.rs
@@ -0,0 +1,89 @@
+use crate::gui_is_not_in_use;
+use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
+use leafwing_input_manager::user_input::InputKind;
+use ryot::prelude::{drawing::*, *};
+use std::marker::PhantomData;
+
+mod draw;
+use draw::draw_to_tile;
+
+mod delete;
+use delete::delete_tile_content;
+
+mod undo_redo;
+use undo_redo::*;
+
+#[derive(Actionlike, Reflect, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum DrawingAction {
+    Stop,
+    Draw,
+    Erase,
+    Undo,
+    Redo,
+}
+
+impl DrawingAction {
+    pub fn get_default_input_map() -> InputMap<DrawingAction> {
+        let mut input_map = InputMap::new([
+            (MouseButton::Left, DrawingAction::Draw),
+            (MouseButton::Right, DrawingAction::Erase),
+        ]);
+
+        input_map.insert_chord([KeyCode::ControlLeft, KeyCode::Z], DrawingAction::Undo);
+        input_map.insert_chord([KeyCode::ControlLeft, KeyCode::R], DrawingAction::Redo);
+
+        // Small hack to remove clash with the pancam plugin
+        input_map.insert_chord(
+            [
+                InputKind::Mouse(MouseButton::Left),
+                InputKind::Keyboard(KeyCode::AltLeft),
+            ],
+            DrawingAction::Stop,
+        );
+
+        input_map
+    }
+}
+
+/// The drawing plugin is responsible for handling the core drawing logic and related commands.
+/// It is also responsible for keeping track of a command history, used to perform undo/redo actions.
+/// The plugin also registers the MapTiles resource, that keeps a map between position and layer in the
+/// map and the entity that represents it.
+pub struct DrawingPlugin<C: ContentAssets>(PhantomData<C>);
+
+impl<C: ContentAssets> DrawingPlugin<C> {
+    pub fn new() -> Self {
+        Self(PhantomData)
+    }
+}
+
+impl<C: ContentAssets> Default for DrawingPlugin<C> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<C: ContentAssets> Plugin for DrawingPlugin<C> {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<UndoRedoConfig>()
+            .init_resource::<CommandHistory>()
+            .init_resource::<MapTiles>()
+            .add_plugins(drawing::DrawingPlugin)
+            .add_plugins(InputManagerPlugin::<DrawingAction>::default())
+            .add_systems(
+                Update,
+                (
+                    draw_to_tile::<C>,
+                    delete_tile_content,
+                    undo_redo_tile_action,
+                )
+                    .run_if(in_state(InternalContentState::Ready))
+                    .run_if(gui_is_not_in_use()),
+            )
+            .add_systems(
+                OnExit(InternalContentState::PreparingSprites),
+                spawn_grid(Color::WHITE),
+            );
+    }
+}
---
+++ ryot_compass/src/bevy_compass/drawing/undo_redo.rs
@@ -0,0 +1,89 @@
+use crate::{Cursor, DrawingAction};
+use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
+use rand::prelude::SliceRandom;
+use rand::thread_rng;
+use ryot::bevy_ryot::*;
+use ryot::prelude::drawing::*;
+
+/// This resource is used to configure the undo/redo system.
+/// Currently, it only contains a timer that is used to control the speed of the undo/redo actions.
+/// The default cooldown for undo/redo is 100ms.
+#[derive(Resource)]
+pub(super) struct UndoRedoConfig {
+    timer: Timer,
+}
+
+impl Default for UndoRedoConfig {
+    fn default() -> Self {
+        Self {
+            timer: Timer::from_seconds(0.1, TimerMode::Repeating),
+        }
+    }
+}
+
+pub(super) fn undo_redo_tile_action(
+    time: Res<Time>,
+    mut commands: Commands,
+    tiles: ResMut<MapTiles>,
+    mut undo_redo_config: ResMut<UndoRedoConfig>,
+    mut command_history: ResMut<CommandHistory>,
+    action_state_query: Query<&ActionState<DrawingAction>, With<Cursor>>,
+) {
+    undo_redo_config.timer.tick(time.delta());
+
+    let action_state = action_state_query.single();
+    let mut actions = [DrawingAction::Undo, DrawingAction::Redo];
+    actions.shuffle(&mut thread_rng());
+
+    for action in actions {
+        let is_timer_finished = undo_redo_config.timer.just_finished();
+
+        if check_action(action, is_timer_finished, action_state) {
+            undo_redo_config.timer.reset();
+            match action {
+                DrawingAction::Undo => undo(&mut commands, &tiles, &mut command_history),
+                DrawingAction::Redo => redo(&mut commands, &tiles, &mut command_history),
+                _ => (),
+            }
+        }
+    }
+}
+
+fn redo(
+    commands: &mut Commands,
+    tiles: &ResMut<MapTiles>,
+    command_history: &mut ResMut<CommandHistory>,
+) {
+    if let Some(command_record) = command_history.reversed_commands.pop() {
+        command_record.command.redo(
+            commands,
+            get_entity_from_command_record(tiles, &command_record),
+        );
+        command_history.performed_commands.push(command_record);
+    }
+}
+
+fn undo(
+    commands: &mut Commands,
+    tiles: &ResMut<MapTiles>,
+    command_history: &mut ResMut<CommandHistory>,
+) {
+    if let Some(command_record) = command_history.performed_commands.pop() {
+        command_record.command.undo(
+            commands,
+            get_entity_from_command_record(tiles, &command_record),
+        );
+        command_history.reversed_commands.push(command_record);
+    }
+}
+
+fn get_entity_from_command_record(
+    tiles: &ResMut<MapTiles>,
+    command_record: &ReversibleCommandRecord,
+) -> Option<Entity> {
+    tiles
+        .get(&command_record.tile_pos)
+        .and_then(|t| t.get(&command_record.layer))
+        .copied()
+}
