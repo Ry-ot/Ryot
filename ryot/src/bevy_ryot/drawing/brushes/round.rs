5f2c4f2c678df0df27ba911e22329cf0b18986ba
Luan Santos <github@luan.sh>
2024-02-11 13:00:52 -0800

    refactor: more flexible brush definitions

--- ryot/src/bevy_ryot/drawing/brushes/round.rs
+++ ryot/src/bevy_ryot/drawing/brushes/round.rs
@@ -1,25 +1,17 @@
 use crate::{bevy_ryot::drawing::brushes::*, include_svg};
 
-#[derive(Clone, Copy)]
 pub struct Round;
 
-impl SelectableTool for Round {
-    fn name(&self) -> &str {
-        "Round"
-    }
-
-    fn icon(&self) -> ImageSource {
-        include_svg!(
-            r##"
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff6c2" viewBox="0 0 256 256"><path d="M232,128A104,104,0,1,1,128,24,104.13,104.13,0,0,1,232,128Z"></path></svg>
-            "##
-        )
-    }
-}
+const NAME: &str = "Round";
+const ICON: ImageSource = include_svg!(
+    r##"
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff6c2" viewBox="0 0 256 256"><path d="M232,128A104,104,0,1,1,128,24,104.13,104.13,0,0,1,232,128Z"></path></svg>
+    "##
+);
 
 impl<B: BrushItem> From<Round> for Brush<B> {
     fn from(_: Round) -> Self {
-        Brush::new(round::<B>)
+        Brush::new(round::<B>, NAME, ICON)
     }
 }
 
--- ryot/src/bevy_ryot/drawing/brushes/square.rs
+++ ryot/src/bevy_ryot/drawing/brushes/square.rs
@@ -1,25 +1,17 @@
 use crate::{bevy_ryot::drawing::brushes::*, include_svg};
 
-#[derive(Clone, Copy)]
 pub struct Square;
+
+const NAME: &str = "Square";
+const ICON: ImageSource = include_svg!(
+    r##"
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff6c2" viewBox="0 0 256 256"><rect x="32" y="32" width="192" height="192" rx="16"></rect></svg>
+    "##
+);
 
 impl<B: BrushItem> From<Square> for Brush<B> {
     fn from(_: Square) -> Self {
-        Brush::new(square::<B>)
-    }
-}
-
-impl SelectableTool for Square {
-    fn name(&self) -> &str {
-        "Square"
-    }
-
-    fn icon(&self) -> ImageSource {
-        include_svg!(
-            r##"
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff6c2" viewBox="0 0 256 256"><rect x="32" y="32" width="192" height="192" rx="16"></rect></svg>
-            "##
-        )
+        Brush::new(square::<B>, NAME, ICON)
     }
 }
 
