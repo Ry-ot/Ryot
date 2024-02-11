014ba1b59ef15a27eb472253ba8b9beac4c289f5
lgrossi <lucas.ggrossi@gmail.com>
2024-02-11 12:51:29 +0100

    fix: Use Res<ActionState> for brush updates

--- ryot_compass/src/bevy_compass/drawing/brush.rs
+++ ryot_compass/src/bevy_compass/drawing/brush.rs
@@ -1,28 +1,24 @@
-use crate::Cursor;
+use crate::{Cursor, DrawingAction};
 use bevy::prelude::*;
 use leafwing_input_manager::prelude::*;
 use ryot::bevy_ryot::drawing::DrawingBundle;
 use ryot::prelude::drawing::*;
 
 pub fn update_brush(
-    mut cursor_query: Query<(
-        &mut Cursor,
-        &ActionState<crate::bevy_compass::drawing::DrawingAction>,
-    )>,
+    action_state: Res<ActionState<DrawingAction>>,
+    mut cursor_query: Query<&mut Cursor>,
     brushes: Res<Brushes<DrawingBundle>>,
 ) {
-    for (mut cursor, action_state) in cursor_query.iter_mut() {
-        if action_state.just_pressed(crate::bevy_compass::drawing::DrawingAction::ChangeBrush) {
+    for mut cursor in cursor_query.iter_mut() {
+        if action_state.just_pressed(DrawingAction::ChangeBrush) {
             cursor.drawing_state.brush_index = brushes.next_index(cursor.drawing_state.brush_index)
         }
 
         let mut size = cursor.drawing_state.brush_size;
 
-        if action_state.just_pressed(crate::bevy_compass::drawing::DrawingAction::IncreaseBrush) {
+        if action_state.just_pressed(DrawingAction::IncreaseBrush) {
             size += 1;
-        } else if action_state
-            .just_pressed(crate::bevy_compass::drawing::DrawingAction::DecreaseBrush)
-        {
+        } else if action_state.just_pressed(DrawingAction::DecreaseBrush) {
             size -= 1;
         }
 
