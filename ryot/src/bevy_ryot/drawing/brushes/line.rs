7c05e7b572312f0076693cbed49f26a58318e2ef
Luan Santos <github@luan.sh>
2024-02-19 00:16:18 -0800

    chore: move egui related code behind feature

--- ryot/src/bevy_ryot/drawing/brushes/line.rs
+++ ryot/src/bevy_ryot/drawing/brushes/line.rs
@@ -1,10 +1,13 @@
-use crate::{bevy_ryot::drawing::brushes::*, include_svg};
+use crate::bevy_ryot::drawing::brushes::*;
+#[cfg(feature = "egui")]
+use crate::include_svg;
 use glam::IVec2;
 
 pub struct Line;
 
 const NAME: &str = "Line";
-const ICON: ImageSource = include_svg!(
+#[cfg(feature = "egui")]
+const ICON: egui::ImageSource = include_svg!(
     r##"
     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff6c2" viewBox="0 0 256 256"><path d="M214.64,86.62a32.07,32.07,0,0,1-38.89,4.94L91.56,175.75a32,32,0,1,1-50.2-6.37h0a32.06,32.06,0,0,1,38.89-4.94l84.19-84.19a32,32,0,1,1,50.2,6.37Z"></path></svg>
     "##
@@ -12,7 +15,12 @@ const ICON: ImageSource = include_svg!(
 
 impl<B: BrushItem> From<Line> for Brush<B> {
     fn from(_: Line) -> Self {
-        Brush::new(line::<B>, NAME, ICON)
+        Brush::new(
+            line::<B>,
+            NAME,
+            #[cfg(feature = "egui")]
+            ICON,
+        )
     }
 }
 
--- ryot/src/bevy_ryot/drawing/brushes/round.rs
+++ ryot/src/bevy_ryot/drawing/brushes/round.rs
@@ -1,9 +1,12 @@
-use crate::{bevy_ryot::drawing::brushes::*, include_svg};
+use crate::bevy_ryot::drawing::brushes::*;
+#[cfg(feature = "egui")]
+use crate::include_svg;
 
 pub struct Round;
 
 const NAME: &str = "Round";
-const ICON: ImageSource = include_svg!(
+#[cfg(feature = "egui")]
+const ICON: egui::ImageSource = include_svg!(
     r##"
     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff6c2" viewBox="0 0 256 256"><path d="M232,128A104,104,0,1,1,128,24,104.13,104.13,0,0,1,232,128Z"></path></svg>
     "##
@@ -11,7 +14,12 @@ const ICON: ImageSource = include_svg!(
 
 impl<B: BrushItem> From<Round> for Brush<B> {
     fn from(_: Round) -> Self {
-        Brush::new(round::<B>, NAME, ICON)
+        Brush::new(
+            round::<B>,
+            NAME,
+            #[cfg(feature = "egui")]
+            ICON,
+        )
     }
 }
 
