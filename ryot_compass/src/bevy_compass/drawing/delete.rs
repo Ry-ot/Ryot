a858ce3f8ffc532ff21864fbfa2e5c74d7f92642
lgrossi <lucas.ggrossi@gmail.com>
2024-02-20 04:44:16 +0100

    feat: primitively add deletion to regular drawing flow

--- ryot_compass/src/bevy_compass/drawing/delete.rs
+++ ryot_compass/src/bevy_compass/drawing/delete.rs
@@ -28,20 +28,36 @@ fn delete_tile_content<F: ReadOnlyWorldQuery>(
     q_current_appearance: Query<(&Visibility, &AppearanceDescriptor), With<TileComponent>>,
 ) {
     for (cursor, tile_pos) in &cursor_query {
-        let top_most_content = brushes(
-            cursor.drawing_state.brush_index,
-            cursor.drawing_state.input_type.into(),
-            DrawingBundle::from_tile_position(*tile_pos),
-        )
+        delete_top_most_elements_in_positions(
+            &brushes(
+                cursor.drawing_state.brush_index,
+                cursor.drawing_state.input_type.into(),
+                DrawingBundle::from_tile_position(*tile_pos),
+            ),
+            &mut commands,
+            &mut command_history,
+            &tiles,
+            &q_current_appearance,
+        );
+    }
+}
+
+pub fn delete_top_most_elements_in_positions(
+    to_delete: &Vec<DrawingBundle>,
+    commands: &mut Commands,
+    command_history: &mut ResMut<CommandHistory>,
+    tiles: &ResMut<MapTiles>,
+    q_current_appearance: &Query<(&Visibility, &AppearanceDescriptor), With<TileComponent>>,
+) {
+    let top_most_content = to_delete
         .iter()
-        .filter_map(|bundle| get_top_most_visible(bundle.tile_pos, &tiles, &q_current_appearance))
+        .filter_map(|bundle| get_top_most_visible(bundle.tile_pos, tiles, q_current_appearance))
         .map(|(_, bundle)| bundle)
         .collect::<Vec<_>>();
 
-        let command = UpdateTileContent::for_new_bundle(top_most_content).revert();
-        commands.add(command.clone());
+    let command = UpdateTileContent::for_new_bundle(top_most_content).revert();
+    commands.add(command.clone());
 
-        command_history.reversed_commands.clear();
-        command_history.performed_commands.push(command.into());
-    }
+    command_history.reversed_commands.clear();
+    command_history.performed_commands.push(command.into());
 }
