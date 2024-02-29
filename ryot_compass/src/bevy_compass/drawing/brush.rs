1c1fec8b7ba236a6976a01109d67257b79383568
lgrossi <lucas.ggrossi@gmail.com>
2024-02-29 14:20:59 +0100

    feat: Move all brush actions behind events

--- ryot_compass/src/bevy_compass/drawing/brush.rs
+++ ryot_compass/src/bevy_compass/drawing/brush.rs
@@ -1,22 +1,27 @@
-use crate::{Cursor, InputType};
+use crate::{Cursor, CursorEvents, InputType};
 use bevy::prelude::*;
 use ryot::bevy_ryot::drawing::DrawingBundle;
 use ryot::prelude::drawing::*;
 
 pub fn change_brush_shape(
-    mut cursor_query: Query<&mut Cursor>,
+    cursor_query: Query<&Cursor>,
     brushes: Res<Brushes<DrawingBundle>>,
+    mut cursor_events_writer: EventWriter<CursorEvents>,
 ) {
-    for mut cursor in cursor_query.iter_mut() {
-        cursor.drawing_state.brush_index = brushes.next_index(cursor.drawing_state.brush_index)
+    for cursor in cursor_query.iter() {
+        let next_brush = brushes.next_index(cursor.drawing_state.brush_index);
+
+        if next_brush != cursor.drawing_state.brush_index {
+            cursor_events_writer.send(CursorEvents::BrushChanged(next_brush));
+        }
     }
 }
 
-pub fn change_brush_size(delta: i32) -> impl FnMut(Query<&mut Cursor>) {
-    move |mut cursor_query: Query<&mut Cursor>| {
-        for mut cursor in cursor_query.iter_mut() {
-            if let InputType::SingleClick(size) = &mut cursor.drawing_state.input_type {
-                *size = (*size + delta).clamp(0, 50);
+pub fn change_brush_size(delta: i32) -> impl FnMut(Query<&Cursor>, EventWriter<CursorEvents>) {
+    move |cursor_query: Query<&Cursor>, mut cursor_events_writer: EventWriter<CursorEvents>| {
+        for cursor in cursor_query.iter() {
+            if let InputType::SingleClick(size) = cursor.drawing_state.input_type {
+                cursor_events_writer.send(CursorEvents::SizeChanged(size + delta));
             }
         }
     }
