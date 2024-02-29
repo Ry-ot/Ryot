1eb3446151ef6091371a2dc95cc0efba5a873729
ramon-bernardo <ramonbernardo.contato@gmail.com>
2024-02-29 00:30:02 -0300

    Use phantom data and impl manual eq, partialEq, copy, clone and debug

--- ryot/src/bevy_ryot/async_events.rs
+++ ryot/src/bevy_ryot/async_events.rs
@@ -1,23 +1,57 @@
 //! This module provides a way to send events between systems asynchronously.
 //! It's useful to send events between threads that perform asynchronous tasks, such as loading
 //! content from disk or loading sprites from a sprite sheet before rendering.
+use std::{hash::Hasher, marker::PhantomData};
+
 use bevy::app::App;
 use bevy::prelude::*;
 use crossbeam_channel::{Receiver, Sender};
 
 // TODO: doc.
-#[derive(SystemSet, Clone, Copy, Eq, PartialEq, Hash, Debug)]
-pub struct AsyncEventSet<T>();
+#[derive(SystemSet, Default)]
+pub struct AsyncEventSet<T: Event>(PhantomData<T>);
+
+impl<T: Event> std::fmt::Debug for AsyncEventSet<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("AsyncEventSet")
+            .field(&format_args!("fn {}()", &std::any::type_name::<T>()))
+            .finish()
+    }
+}
+
+impl<T: Event> std::hash::Hash for AsyncEventSet<T> {
+    fn hash<H: Hasher>(&self, _state: &mut H) {
+        // all systems of a given type are the same
+    }
+}
+
+impl<T: Event> Clone for AsyncEventSet<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T: Event> Copy for AsyncEventSet<T> {}
+
+impl<T: Event> PartialEq for AsyncEventSet<T> {
+    #[inline]
+    fn eq(&self, _other: &Self) -> bool {
+        // all systems of a given type are the same
+        true
+    }
+}
+
+impl<T: Event> Eq for AsyncEventSet<T> {}
 
 /// A resource that emits asynchronous events of a given type.
 /// It's a bevy friendly wrapper around a `crossbeam_channel::Sender`.
 #[derive(Resource, Deref, DerefMut)]
-pub struct AsyncEventSender<T>(pub Sender<T>);
+pub struct AsyncEventSender<T: Event>(pub Sender<T>);
 
 /// A resource that receives asynchronous events of a given type.
 /// It's a bevy friendly wrapper around a `crossbeam_channel::Receiver`.
 #[derive(Resource, Deref, DerefMut)]
-struct AsyncEventReceiver<T>(Receiver<T>);
+struct AsyncEventReceiver<T: Event>(Receiver<T>);
 
 /// A trait to add an asynchronous event to an App.
 pub trait AsyncEventApp {
@@ -35,17 +69,23 @@ impl AsyncEventApp for App {
 
         let (sender, receiver) = crossbeam_channel::unbounded::<T>();
 
-		self.add_event::<T>()
-            .add_systems(PreUpdate, unbounded_channel_to_event::<T>.in_set(AsyncEventSet<T>()))
+        self.add_event::<T>()
+            .add_systems(
+                PreUpdate,
+                unbounded_channel_to_event::<T>.in_set(AsyncEventSet::<T>(PhantomData)),
+            )
             .insert_resource(AsyncEventSender(sender))
             .insert_resource(AsyncEventReceiver(receiver));
 
-		self
+        self
     }
 }
 
 /// A system that sends events from the receiver to Bevy's event system.
 /// Converts the asynchronous event into a bevy event and sends it to the event system.
-fn unbounded_channel_to_event<T: Event>(receiver: Res<AsyncEventReceiver<T>>, mut writer: EventWriter<T>) {
+fn unbounded_channel_to_event<T: Event>(
+    receiver: Res<AsyncEventReceiver<T>>,
+    mut writer: EventWriter<T>,
+) {
     writer.send_batch(receiver.try_iter());
 }
