05e08a7b81e831f187d66a0c9ea1ba87f1484816
lgrossi <lucas.ggrossi@gmail.com>
2024-03-29 22:42:19 +0100

    impr: Add position is_directly_connected

--- ryot/src/bevy_ryot/drawing/brushes/line.rs
+++ ryot/src/bevy_ryot/drawing/brushes/line.rs
@@ -37,38 +37,9 @@ pub fn line<B: BrushItem>(params: BrushParams<B>, center: B) -> Vec<B> {
         BrushParams::Element(e) => e.get_position(),
     };
 
-    for pos in bresenhams_line(start_pos, center_pos) {
+    for pos in start_pos.bresenhams_line(center_pos) {
         elements.push(B::from_position(center, pos));
     }
 
     elements
-}
-
-fn bresenhams_line(a: TilePosition, b: TilePosition) -> Vec<TilePosition> {
-    let mut points = Vec::new();
-
-    let mut current = a;
-    let dx = (b.x - a.x).abs();
-    let dy = -(b.y - a.y).abs();
-    let sx = if a.x < b.x { 1 } else { -1 };
-    let sy = if a.y < b.y { 1 } else { -1 };
-    let mut err = dx + dy;
-
-    loop {
-        points.push(current);
-        if current == b {
-            break;
-        }
-        let e2 = 2 * err;
-        if e2 >= dy {
-            err += dy;
-            current.x += sx;
-        }
-        if e2 <= dx {
-            err += dx;
-            current.y += sy;
-        }
-    }
-
-    points
 }
