4d3a91bd39a567a80f3135cd47c8fbdb3fc4dcdd
lgrossi <lucas.ggrossi@gmail.com>
2024-04-18 09:01:03 +0200

    chore: move compression to ryot_core

--- crates/ryot_core/src/lib.rs
+++ crates/ryot_core/src/lib.rs
@@ -2,12 +2,17 @@
 pub mod cache;
 
 pub mod async_task;
+#[cfg(feature = "compression")]
+pub mod compression;
 
 pub mod prelude {
     pub use crate::{async_task::execute, is_true, Flag};
 
     #[cfg(feature = "bevy")]
     pub use crate::cache::{Cache, CacheSystems};
+
+    #[cfg(feature = "compression")]
+    pub use crate::compression::{compress, decompress, Compression, Zstd};
 }
 
 pub fn is_true(value: Option<bool>) -> bool {
