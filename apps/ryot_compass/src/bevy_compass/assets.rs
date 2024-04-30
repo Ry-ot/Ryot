f6dc79955dce43d2edb6900b165d361dd051eb1e
lgrossi <lucas.ggrossi@gmail.com>
2024-04-30 14:39:01 +0200

    feat: add macro for ryot assets

--- apps/ryot_compass/src/bevy_compass/assets.rs
+++ apps/ryot_compass/src/bevy_compass/assets.rs
@@ -2,32 +2,10 @@ use bevy::prelude::*;
 use bevy_asset_loader::asset_collection::AssetCollection;
 use ryot::prelude::*;
 
-#[derive(AssetCollection, Resource, Default)]
-pub struct CompassContentAssets {
-    #[asset(key = "layouts", collection(typed))]
-    atlas_layout: Vec<Handle<TextureAtlasLayout>>,
-    #[asset(path = "appearances.dat")]
-    visual_elements: Handle<VisualElements>,
-    #[asset(path = "catalog-content.json")]
-    catalog_content: Handle<Catalog>,
-    #[asset(path = "ryot_mascot.png")]
-    pub mascot: Handle<Image>,
-}
-
-impl CatalogAsset for CompassContentAssets {
-    fn catalog_content(&self) -> &Handle<Catalog> {
-        &self.catalog_content
+ryot_asset!(
+    CompassContentAssets,
+    {
+        #[asset(path = "ryot_mascot.png")]
+        pub mascot: Handle<Image>,
     }
-}
-
-impl VisualElementsAsset for CompassContentAssets {
-    fn visual_elements(&self) -> &Handle<VisualElements> {
-        &self.visual_elements
-    }
-}
-
-impl AtlasLayoutsAsset for CompassContentAssets {
-    fn atlas_layouts(&self) -> &Vec<Handle<TextureAtlasLayout>> {
-        &self.atlas_layout
-    }
-}
+);
--- crates/ryot_assets/src/atlas.rs
+++ crates/ryot_assets/src/atlas.rs
@@ -1,8 +1,6 @@
 use bevy_asset::Handle;
-use bevy_asset_loader::asset_collection::AssetCollection;
-use bevy_ecs::prelude::Resource;
 use bevy_sprite::TextureAtlasLayout;
 
-pub trait AtlasLayoutsAsset: Resource + AssetCollection + Send + Sync + 'static {
+pub trait AtlasLayoutsAsset: crate::RyotAsset {
     fn atlas_layouts(&self) -> &Vec<Handle<TextureAtlasLayout>>;
 }
