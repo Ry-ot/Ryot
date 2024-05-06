f2aa301235678284ee674f1cd0feb4677ca7c241
lgrossi <lucas.ggrossi@gmail.com>
2024-05-06 02:20:54 +0200

    fix: some bumps and version fixes

--- crates/ryot_trajectories/src/lib.rs
+++ crates/ryot_trajectories/src/lib.rs
@@ -1,14 +1,8 @@
-//! `ryot_trajectories`
-//!
-//! Provides trajectory capabilities specifically for Bevy 2D, essential for implementing
-//! line-of-sight features and other interactive game mechanics.
-//!
-//! It includes functionalities for managing and processing perspectives and visibility
-//! of entities in a game environment. Perspectives are defined by sets of view points that
-//! determine what an entity can see, based on it spatial position and other considerations.
+#![doc = include_str!("../README.md")]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![feature(trait_alias)]
 use bevy_math::bounding::Aabb3d;
-use ryot_core::game::Point;
+use ryot_core::prelude::Point;
 
 mod app;
 mod collision;
